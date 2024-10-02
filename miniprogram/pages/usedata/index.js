77e10f7e1a5c3f3f0bb09834a30d2657e43e89a6
xiaoleiidanzi <291194546@qq.com>
2024-10-02 21:00:03 +0800

    修改记录页面广告显示范围，增加实时日志

--- miniprogram/pages/usedata/index.js
+++ miniprogram/pages/usedata/index.js
@@ -1,21 +1,43 @@
 var app_var= getApp()
+const logManager = wx.getRealtimeLogManager()
+const logger = logManager.tag('plugin-onUserTapSth')
+
 Page({
   data:{
-      list:[ ]
+      list:[ ],
+      contentId: 0,//验证码开关，0：不显示、1：显示
   },
   adLoad() {
+    var that = this; // 保存页面上下文
     console.log('原生模板广告加载成功')
+    logger.info('adLoad', '原生模板广告加载成功')
+    that.setData({
+      contentId: 1,
+      //datadb:res.data
+    });
   },
   adError(err) {
+    var that = this; // 保存页面上下文
     console.error('原生模板广告加载失败', err)
+    logger.error('adError', err)
+    that.setData({
+      contentId: 1,
+      //datadb:res.data
+    });
   },
   adClose() {
+    var that = this; // 保存页面上下文
     console.log('原生模板广告关闭')
+    logger.info('adClose', '原生模板广告关闭')
+    that.setData({
+      contentId: 1,
+      //datadb:res.data
+    });
   },
   
   onLoad(){
-    console.log('按钮被点击了');
-    console.log('333'+app_var.globalData.openid);
+    logger.info('onLoad-openid', app_var.globalData.openid)
+    console.log(app_var.globalData.openid);
     var that = this; // 保存页面上下文
     wx.request({
       url: 'https://xiaoleidianzi.xyz/xiaochengxu/getlog.php',
@@ -32,19 +54,25 @@ Page({
 
       success: function (res) {
           console.log(res.data)
-          
+          logger.info('onLoad-success', res.data)
         // 将获取的数据存储到页面数据中
-        that.setData({
-            contentId: 1,
+          that.setData({
+            //contentId: 1,
             datadb:res.data
-        });
+          });
+        
       },
       fail: function (error) {
         console.error('请求失败', error);
+        logger.error('onLoad-fail', error)
+        that.setData({
+          //contentId: 1,
+          datadb:'"date":"获取失败","code":"获取失败"'
+        });
       }
     });
 
-    console.log('页面跳转了');
+    
 
 }
 })
